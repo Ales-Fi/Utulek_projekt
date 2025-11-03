a53a40ccc19533903c5ee5acc91aaa99c515784b
Ales-Fi <fiala.ales2000@gmail.com>
2025-11-03 08:31:34 +0100

    VYtvoření strukutry metod

--- Utulek_projekt/Services/Evidence.cs
+++ Utulek_projekt/Services/Evidence.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utulek_projekt.Model;
+using Utulek_projekt.UI;
 
 namespace Utulek_projekt.Services
 {
     internal class Evidence
     {
+        public new List<Zvire> Zvirata = new List<Zvire>();
+
+        public void PridejZvire()
+        { 
+            
+        }
+        public void UpravitZvire()
+        {
+
+        }
+        public void SmazatZvire()
+        {
+
+        }
+        public void VypsatZvire()
+        {
+
+        }
+
+        public void Adopce()
+        { 
+        
+        }
+
     }
 }
