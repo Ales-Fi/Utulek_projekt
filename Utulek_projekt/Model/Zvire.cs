30f1ef7503b3c7b4e714d5550e513f1ad718bbec
Ales-Fi <fiala.ales2000@gmail.com>
2025-11-03 08:38:18 +0100

    Uprava atributu

--- Utulek_projekt/Model/Zvire.cs
+++ Utulek_projekt/Model/Zvire.cs
@@ -12,7 +12,7 @@ namespace Utulek_projekt.Model
         public string Jmeno { get; private set; }
         public string Druh { get; private set; }
         public int Vek { get; private set; }
-        public string Pohlavi { get; private set; } = 0;
+        public string Pohlavi { get; private set; };
         public bool Adoptovano { get; private set; } = false;
     }
 }
