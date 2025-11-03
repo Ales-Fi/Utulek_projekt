809ed48a9372e59df7c6e2c761e7baece15466f4
UsedMrBanan2 <lukaskam07@gmail.com>
2025-11-03 08:24:43 +0100

    vytvoreni vlastnosti

--- Utulek_projekt/Program.cs
+++ Utulek_projekt/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace Utulek_projekt
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        public string Jmeno { get; private set; }
+        public string Druh { get; private set; }
+        public int Vek {  get; private set; }
+        public string Pohlavi { get; private set; } = 0;
+        public bool Adoptovano { get; private set; } = false;
     }
 }
