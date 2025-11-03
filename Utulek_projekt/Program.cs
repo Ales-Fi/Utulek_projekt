a0e85a46f0ffdbfa86dca89ce8fcdaaacd68f74f
UsedMrBanan2 <lukaskam07@gmail.com>
2025-11-03 08:32:42 +0100

    vlastnosti zvire

--- Utulek_projekt/Program.cs
+++ Utulek_projekt/Program.cs
@@ -9,10 +9,6 @@ namespace Utulek_projekt
 {
     internal class Program
     {
-        public string Jmeno { get; private set; }
-        public string Druh { get; private set; }
-        public int Vek {  get; private set; }
-        public string Pohlavi { get; private set; } = 0;
-        public bool Adoptovano { get; private set; } = false;
+     
     }
 }
